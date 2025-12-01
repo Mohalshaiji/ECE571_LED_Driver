bcb571c553961073a70d930f30d3d4a61aadc442
liukee-559 <liukee@pdx.edu>
2025-12-01 15:11:29 -0800

    Update led_driver.sv

--- FULL SYS/led_driver.sv
+++ FULL SYS/led_driver.sv
@@ -34,6 +34,10 @@ module led_driver(
     logic  [7:0] i2c_byte;       // received I2C byte
     logic        i2c_byte_valid; // pulse when a new byte is available
 
+    logic   [7:0] tx_data;       // transmiting byte on I2C bus
+    logic         tx_req;        // one-cycle pulse requesting the interface to send tx_data 
+    logic         tx_ready;      // indicates that the I2C interface is ready to accept a new tx_data byte
+    
     // Register interface between I2C controller and LED controller
     logic [2:0] reg_addr;        // 3-bit address (0x0–0x7)
     logic [7:0] reg_wdata;       // data to write
@@ -42,7 +46,23 @@ module led_driver(
     logic       reg_read;        // read strobe
 
 
-
     //-- I2C --
 
+
+    // I2C bus interface module(Liukee)
+    i2c_bus_interface u_i2c_bus(
+        .clk        (clk_osc),
+        .reset      (reset),
+        .SDA        (sda),
+        .SCL        (scl),
+
+        .rx_data    (i2c_byte), 
+        .rx_valid   (i2c_byte_valid),
+
+        .tx_data    (tx_data),
+        .tx_req     (tx_req),
+        .tx_ready   (tx_ready)
+    ）；
+
+    // I2C controller module
 endmodule
